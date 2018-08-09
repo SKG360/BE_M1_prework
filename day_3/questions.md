c1e7021b0c7d75734d199843024e66e07412db81
SKG360 <samuel.giles@colorado.edu>
2018-08-09 08:13:32 -0600

    Adds Day 3 and Day 4 files

---
+++ day_3/questions.md
@@ -0,0 +1,35 @@
+## Day 3 Questions
+
+1. What is a conditional statement? Give three examples.
+
+conditional statements evaluate to true or false
+
+1. Why might you want to use an if-statement?
+
+to control conditional instructions
+
+1. What is the Ruby syntax for an if statement?
+
+if boolean
+
+end
+
+1. How do you add multiple conditions to an if statement?
+
+if boolean
+  else ...
+end
+
+1. What is the Ruby syntax for an if/elsif/else statement?
+
+if boolean
+  elsif ...
+  elsif ...
+  else ...
+end
+
+1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+unless boolean
+
+end 
---
+++ day_4/exercises/methods.rb
@@ -0,0 +1,35 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_4/exercises/methods.rb`
+
+# example: Write a method below that, when called will print your name
+def print_name
+  p "Severus Snape"
+end
+
+print_name
+
+# Write a method that takes an argument of your name and prints your name
+def print_name(name)
+  p "Hello #{name}!"
+end
+
+print_name("Albus Dumbledore")
+
+# Write a method that takes in 2 numbers and prints their sum, then call that
+# method.
+def two_numbers(num1, num2)
+  p num1 + num2
+end
+
+two_numbers(5, 9)
+
+# Write a method that takes in two strings and prints a concatenation
+# of those two strings, for example the arguments could be (man, woman) and
+# the end result might be "When Harry Met Sally".  Then, call that method.
+
+def harry_sally(man, woman)
+  p man + woman
+end
+
+harry_sally("When Harry", "Met Sally")
